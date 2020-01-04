d8bedba978620d38ed9e0ad47915d7faf1258b68
Keegan Mullaney <keegan@kmauthorized.com>
2020-01-03 17:02:50 -0800

    install express and add index.js
    The package-lock.json file will ensure that other developers get the
    same versions of npm packages and have the same dev environment.

---
+++ index.js
@@ -0,0 +1,18 @@
+// import express
+const express = require('express')
+
+// create new express app and assign it to `app` constant
+const app = express()
+
+// server port configuration
+const PORT = 8080
+
+// create a route for the app
+app.get('/', (req, res) => {
+  res.send('Hello dev.to!')
+})
+
+// start server listening on `PORT`
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`)
+})
